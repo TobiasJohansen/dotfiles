4b100c0977802686064bf1bbcaf0e4d8349c29ef
Tobias Johansen <tobjoh1994@gmail.com>
2026-03-10 17:46:37 +0100

    specify branch for nvim-treesitter

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -892,6 +892,7 @@ require('lazy').setup({
 
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
+    branch = 'main',
     lazy = false,
     build = ':TSUpdate',
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter-intro`
